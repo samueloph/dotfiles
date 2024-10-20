df0cf2ba3a5ee023f14d9b58f86d30ffce56ce9f
Samuel Henrique <samueloph@debian.org>
2024-10-20 22:20:17 +0100

    update nvim config

--- nvim/supporting_files/nvim/lua/samueloph/lazy/lsp.lua
+++ nvim/supporting_files/nvim/lua/samueloph/lazy/lsp.lua
@@ -28,7 +28,7 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
-                "tsserver",
+                "ts_ls",
                 "bashls",
                 "pyright",
             },
