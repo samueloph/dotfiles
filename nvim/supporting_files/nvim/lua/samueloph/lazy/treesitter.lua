72aac39c09e3c99c82c7c52213571d4e327f1d4b
Samuel Henrique <samueloph@debian.org>
2024-11-06 19:18:33 +0000

    treesitter.nvim: Install diff parser

--- nvim/supporting_files/nvim/lua/samueloph/lazy/treesitter.lua
+++ nvim/supporting_files/nvim/lua/samueloph/lazy/treesitter.lua
@@ -6,7 +6,7 @@ return {
             -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash", "python", "json", "yaml",
+                "jsdoc", "bash", "python", "json", "yaml", "diff"
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
