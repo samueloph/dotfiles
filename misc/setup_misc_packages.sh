b64ddfbe5534cd8f4bab53b23ac5fc3558074f6b
Samuel Henrique <samueloph@debian.org>
2025-07-01 21:35:24 +0100

    setup_misc_packages: Install steam

--- misc/setup_misc_packages.sh
+++ misc/setup_misc_packages.sh
@@ -18,5 +18,16 @@ setup_git(){
 
     apt_install_wrapper "${PACKAGE_LIST_MISC_PACKAGES[@]}"
 
+
+    if [[ $(dpkg --print-foreign-architectures) != "i386" ]]; then
+        print_in_progress "Enabling i386 arch in dpkg, for Steam"
+        sudo dpkg --add-architecture i386
+        sudo apt update
+    else
+        print_skip "i386 arch is already enabled in dpkg"
+    fi
+
+    apt_install_wrapper steam-installer
+
     print_header "[/MISC PACKAGES]"
 }
